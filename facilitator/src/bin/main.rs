f95cd4ad76ef367e851fec49e0c736db3baad696
Tim Geoghegan <timg@letsencrypt.org>
2020-09-12 15:50:29 -0700

    Sample: write ingestion header
    Implements all(?) the necessary command line flags and writes an Avro
    ingestion header to the specified path.

--- facilitator/src/bin/main.rs
+++ facilitator/src/bin/main.rs
@@ -1,11 +1,44 @@
+use chrono::prelude::*;
+use chrono::DateTime;
 use clap::{App, Arg, SubCommand};
-use std::process;
+use facilitator::sample::generate_ingestion_sample;
+use facilitator::transport::FileTransport;
+use facilitator::Error;
+use libprio_rs::encrypt::PrivateKey;
+use std::path::Path;
+use uuid::Uuid;
 
-fn integer_validator(s: String) -> Result<(), String> {
+const DATE_FORMAT: &str = "%Y/%m/%d/%H/%M";
+const DEFAULT_PHA_PRIVATE_KEY: &str =
+    "BIl6j+J6dYttxALdjISDv6ZI4/VWVEhUzaS05LgrsfswmbLOgNt9HUC2E0w+9Rq\
+    Zx3XMkdEHBHfNuCSMpOwofVSq3TfyKwn0NrftKisKKVSaTOt5seJ67P5QL4hxgPWvxw==";
+const DEFAULT_FACILITATOR_PRIVATE_KEY: &str =
+    "BNNOqoU54GPo+1gTPv+hCgA9U2ZCKd76yOMrWa1xTWgeb4LhFLMQIQoRwDVaW64g\
+    /WTdcxT4rDULoycUNFB60LER6hPEHg/ObBnRPV1rwS3nj9Bj0tbjVPPyL9p8QW8B+w==";
+
+fn usize_validator(s: String) -> Result<(), String> {
     s.parse::<usize>().map(|_| ()).map_err(|e| e.to_string())
 }
 
-fn main() {
+fn f64_validator(s: String) -> Result<(), String> {
+    s.parse::<f64>().map(|_| ()).map_err(|e| e.to_string())
+}
+
+fn date_validator(s: String) -> Result<(), String> {
+    DateTime::parse_from_str(&s, DATE_FORMAT)
+        .map(|_| ())
+        .map_err(|e| e.to_string())
+}
+
+fn b64_validator(s: String) -> Result<(), String> {
+    base64::decode(s).map(|_| ()).map_err(|e| e.to_string())
+}
+
+fn uuid_validator(s: String) -> Result<(), String> {
+    Uuid::parse_str(&s).map(|_| ()).map_err(|e| e.to_string())
+}
+
+fn main() -> Result<(), Error> {
     let matches = App::new("facilitator")
         .about("Prio facilitator server")
         // Environment variables are injected via build.rs
@@ -33,27 +66,164 @@ fn main() {
                         .help("Directory to write sample data into"),
                 )
                 .arg(
+                    Arg::with_name("aggregation-id")
+                        .long("aggregation-id")
+                        .value_name("ID")
+                        .default_value("fake-aggregation")
+                        .help("Aggregation ID to use when constructing object keys"),
+                )
+                .arg(
+                    Arg::with_name("batch-id")
+                        .long("batch-id")
+                        .value_name("UUID")
+                        .help("Batch ID to use when constructing object keys")
+                        .long_help(
+                            "Batch ID to use when constructing object keys. If omitted, a \
+                            UUID is generated.",
+                        )
+                        .validator(uuid_validator),
+                )
+                .arg(
+                    Arg::with_name("date")
+                        .long("date")
+                        .value_name("DATE")
+                        .help("Date to use when constructing object keys in YYYYmmddHHMM format")
+                        .long_help(
+                            "Date to use when constructing object keys. If omitted, the current \
+                        time is used.",
+                        )
+                        .validator(date_validator),
+                )
+                .arg(
                     Arg::with_name("dimension")
                         .long("dimension")
                         .short("d")
-                        .value_name("DIM")
+                        .value_name("INT")
                         .default_value("123")
-                        .validator(integer_validator)
-                        .help("Dimension parameter. Must be an integer."),
+                        .validator(usize_validator)
+                        .help("Length in bits of the data packets to generate"),
+                )
+                .arg(
+                    Arg::with_name("packet-count")
+                        .long("packet-count")
+                        .short("p")
+                        .value_name("INT")
+                        .default_value("10")
+                        .validator(usize_validator)
+                        .help("Number of data packets to generate"),
+                )
+                .arg(
+                    Arg::with_name("pha-private-key")
+                        .long("pha-private-key")
+                        .value_name("B64")
+                        .help("Base64 encoded private key for the PHA server")
+                        .long_help("If not specified, a fixed private key will be used.")
+                        .default_value(DEFAULT_PHA_PRIVATE_KEY)
+                        .hide_default_value(true)
+                        .validator(b64_validator),
+                )
+                .arg(
+                    Arg::with_name("facilitator-private-key")
+                        .long("facilitator-private-key")
+                        .value_name("B64")
+                        .help("Base64 encoded private key for the facilitator server")
+                        .long_help("If not specified, a fixed private key will be used.")
+                        .default_value(DEFAULT_FACILITATOR_PRIVATE_KEY)
+                        .hide_default_value(true)
+                        .validator(b64_validator),
+                )
+                .arg(
+                    Arg::with_name("bins")
+                        .long("bins")
+                        .value_name("INT")
+                        .help("Number of bins for the aggregation")
+                        .default_value("100")
+                        .validator(usize_validator),
+                )
+                .arg(
+                    Arg::with_name("epsilon")
+                        .long("epsilon")
+                        .value_name("DOUBLE")
+                        .help(
+                            "Differential privacy parameter for local randomization before \
+                            aggregation",
+                        )
+                        .default_value("0.23")
+                        .validator(f64_validator),
+                )
+                .arg(
+                    Arg::with_name("batch-start-time")
+                        .long("batch-start-time")
+                        .value_name("MILLIS")
+                        .help("Start of timespan covered by the batch, in milliseconds since epoch")
+                        .default_value("1000000000")
+                        .validator(usize_validator),
+                )
+                .arg(
+                    Arg::with_name("batch-end-time")
+                        .long("batch-end-time")
+                        .value_name("MILLIS")
+                        .help("End of timespan covered by the batch, in milliseconds since epoch")
+                        .default_value("1000000100")
+                        .validator(usize_validator),
                 ),
         )
         .get_matches();
 
-    let verbose = matches.is_present("verbose");
-
-    if verbose {
-        println!("verbose output is on");
-    }
+    let _verbose = matches.is_present("verbose");
 
     match matches.subcommand() {
         ("generate-ingestion-sample", Some(sub_matches)) => {
-            println!("subcommand generate sample {:?}", sub_matches)
+            // The configuration of the Args above should guarantee that the
+            // various parameters are present and valid, so it is safe to use
+            // unwrap() here.
+            generate_ingestion_sample(
+                Box::new(FileTransport::new(
+                    Path::new(sub_matches.value_of("output").unwrap()).to_path_buf(),
+                )),
+                sub_matches
+                    .value_of("batch-uuid")
+                    .map_or_else(|| Uuid::new_v4(), |v| Uuid::parse_str(v).unwrap()),
+                sub_matches.value_of("aggregation-id").unwrap().to_owned(),
+                sub_matches.value_of("date").map_or_else(
+                    || Utc::now().format(DATE_FORMAT).to_string(),
+                    |v| v.to_string(),
+                ),
+                &PrivateKey::from_base64(sub_matches.value_of("pha-private-key").unwrap()).unwrap(),
+                &PrivateKey::from_base64(sub_matches.value_of("facilitator-private-key").unwrap())
+                    .unwrap(),
+                sub_matches
+                    .value_of("dimension")
+                    .unwrap()
+                    .parse::<usize>()
+                    .unwrap(),
+                sub_matches
+                    .value_of("packet-count")
+                    .unwrap()
+                    .parse::<usize>()
+                    .unwrap(),
+                sub_matches
+                    .value_of("bins")
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap(),
+                sub_matches
+                    .value_of("epsilon")
+                    .unwrap()
+                    .parse::<f64>()
+                    .unwrap(),
+                sub_matches
+                    .value_of("batch-start-time")
+                    .unwrap()
+                    .parse::<i64>()
+                    .unwrap(),
+                sub_matches
+                    .value_of("batch-end-time")
+                    .unwrap()
+                    .parse::<i64>()
+                    .unwrap(),
+            )
         }
-        _ => println!("no subcommand provided"),
+        (_, _) => Ok(()),
     }
 }
--- facilitator/src/transport.rs
+++ facilitator/src/transport.rs
@@ -1,11 +1,11 @@
 use crate::Error;
-use std::fs::File;
+use std::fs::{create_dir_all, File};
 use std::io::{BufReader, BufWriter, Read, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 pub trait Transport {
-    fn get(&self, key: &str) -> Result<Box<dyn Read>, Error>;
-    fn put(&self, key: &str) -> Result<Box<dyn Write>, Error>;
+    fn get(&self, key: &Path) -> Result<Box<dyn Read>, Error>;
+    fn put(&self, key: &Path) -> Result<Box<dyn Write>, Error>;
 }
 
 pub struct FileTransport {
@@ -21,15 +21,25 @@ impl FileTransport {
 }
 
 impl Transport for FileTransport {
-    fn get(&self, key: &str) -> Result<Box<dyn Read>, Error> {
+    fn get(&self, key: &Path) -> Result<Box<dyn Read>, Error> {
         let path = self.directory.join(key);
-        let f = File::open(path.as_path()).map_err(|e| Error::IoError(e))?;
+        let f = File::open(path.as_path())
+            .map_err(|e| Error::IoError(e, format!("opening {}", path.display())))?;
         Ok(Box::new(BufReader::new(f)))
     }
 
-    fn put(&self, key: &str) -> Result<Box<dyn Write>, Error> {
+    fn put(&self, key: &Path) -> Result<Box<dyn Write>, Error> {
         let path = self.directory.join(key);
-        let f = File::create(path.as_path()).map_err(|e| Error::IoError(e))?;
+        if let Some(parent) = path.parent() {
+            create_dir_all(parent).map_err(|e| {
+                Error::IoError(
+                    e,
+                    format!("creating parent directories {}", parent.display()),
+                )
+            })?;
+        }
+        let f = File::create(path.as_path())
+            .map_err(|e| Error::IoError(e, format!("creating {}", path.display())))?;
         Ok(Box::new(BufWriter::new(f)))
     }
 }
@@ -44,13 +54,13 @@ mod tests {
     fn roundtrip_file_transport() {
         let tempdir = tempfile::TempDir::new().unwrap();
         let file_transport = FileTransport::new(tempdir.path().to_path_buf());
-        let writer = file_transport.put("foo");
+        let writer = file_transport.put(Path::new("foo"));
         assert!(writer.is_ok(), "put failed: {:?}", writer.err());
 
         let res = writer.unwrap().write(b"some content");
         assert!(res.is_ok(), "write failed: {:?}", res.err());
 
-        let reader = file_transport.get("foo");
+        let reader = file_transport.get(Path::new("foo"));
         assert!(reader.is_ok(), "get failed: {:?}", reader.err());
 
         let mut contents = String::new();
