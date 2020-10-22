3b09ac535fbcf5b1817fc2d2f491ec1fc7fafa97
Yuri Kunde Schlesner <yuriks@letsencrypt.org>
2020-10-22 16:03:40 -0700

    facilitator: Add env_logger as a logging backend

--- facilitator/src/bin/workflow-manager.rs
+++ facilitator/src/bin/workflow-manager.rs
@@ -1,7 +1,10 @@
+use env_logger::Env;
 use facilitator::WorkflowArgs;
 use structopt::StructOpt;
 
 fn main() -> Result<(), anyhow::Error> {
+    env_logger::init_from_env(Env::new().default_filter_or("warn"));
+
     let args = WorkflowArgs::from_args();
     facilitator::workflow_main(args)
 }
