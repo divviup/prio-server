e337fbf461e34daa0fb39324fa8a8a48246223ae
Amir Omidi <amir@letsencrypt.org>
2020-10-27 12:54:29 -0700

    Add some more comments

--- terraform-parser/kubernetes/kubernetes.go
+++ terraform-parser/kubernetes/kubernetes.go
@@ -1,5 +1,6 @@
 package kubernetes
 
+// TerraformData is the representation of the CRD defined by the Operator
 type TerraformData struct {
 	ApiVersion string `json:"apiVersion"`
 	Kind       string `json:"kind"`
@@ -7,11 +8,13 @@ type TerraformData struct {
 	Spec       `json:"spec"`
 }
 
+// Metadata is the representation of the metadata of the CRD
 type Metadata struct {
 	Name      string `json:"name"`
 	Namespace string `json:"namespace"`
 }
 
+// Spec is the representation of the spec of the CRD
 type Spec struct {
 	CertificateFQDN     string `json:"certificateFQDN"`
 	HealthAuthorityName string `json:"healthAuthorityName"`
