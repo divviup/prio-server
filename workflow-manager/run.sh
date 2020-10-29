1d6d90d1773231a05137c76ba5ce8a78bfb238b9
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-10-29 16:09:00 -0700

    Remove excess files

--- workflow-manager/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-N="-n jsha-peer-1"
-J=peer-1-baku-manual
-kubectl $N delete job $J
-kubectl $N create job --from=cronjob/jsha-fridemo-jsha-peer-1-baku-workflow-manager $J 
-sleep 10
-kubectl $N logs --follow jobs/$J
