63a152e3718be6fe204d42d2681bd0e763dfec54
Amir Omidi <amir@letsencrypt.org>
2020-11-16 23:22:19 -0800

    Add build scripts for deploy operator and manifest updater

---
+++ deploy-operator/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eux
+cd $(dirname $0)
+
+# Embed info about the build.
+COMMIT_ID="$(git rev-parse --short=8 HEAD)"
+BUILD_ID="$(git symbolic-ref --short HEAD 2>/dev/null || true)+${COMMIT_ID}"
+BUILD_TIME="$(date)"
+BUILD_INFO="${BUILD_ID} - ${BUILD_TIME}"
+
+docker build --tag letsencrypt/prio-deploy-operator --build-arg BUILD_INFO="${BUILD_INFO}" .
