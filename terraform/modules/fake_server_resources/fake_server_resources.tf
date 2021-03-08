4556063e8fa93f2eaa31f0090d4417f1df109f19
Amir Omidi <amir@letsencrypt.org>
2021-03-07 21:05:51 -0500

    Terraform changes to allow for integration testing (#463)
    * Fixes some things that were missed
    
    * Changes the portal manifest for the pha instance to point at the one built in facilitator
    
    * Portal server needs to be made for both processors

--- terraform/modules/fake_server_resources/fake_server_resources.tf
+++ terraform/modules/fake_server_resources/fake_server_resources.tf
@@ -10,10 +10,6 @@ variable "manifest_bucket" {
   type = string
 }
 
-variable "sum_part_bucket_writer_email" {
-  type = string
-}
-
 variable "peer_manifest_base_url" {
   type = string
 }
@@ -50,49 +46,6 @@ variable "facilitator_version" {
   type = string
 }
 
-
-
-# For our purposes, a fake portal server is simply a bucket where we can write
-# sum parts, as well as a correctly formed global manifest advertising that
-# bucket's name.
-resource "google_storage_bucket" "sum_part_output" {
-  name     = "prio-${var.environment}-sum-part-output"
-  location = var.gcp_region
-  # Force deletion of bucket contents on bucket destroy. Bucket contents would
-  # be re-created by a subsequent deploy so no reason to keep them around.
-  force_destroy = true
-  # Disable per-object ACLs. Everything we put in here is meant to be world-
-  # readable and this is also in line with Google's recommendation:
-  # https://cloud.google.com/storage/docs/uniform-bucket-level-access
-  uniform_bucket_level_access = true
-}
-
-# Enable the sum part bucket writer GCP SA to write output
-resource "google_storage_bucket_iam_binding" "write_sum_parts" {
-  bucket = google_storage_bucket.sum_part_output.name
-  # Allow ourselves to write to sum part outputs
-  role = "roles/storage.objectAdmin"
-  members = [
-    "serviceAccount:${var.sum_part_bucket_writer_email}"
-  ]
-}
-
-# Create a portal server global manifest and advertise it from our manifest
-# bucket. Note that the manifest bucket name and the relative path in this
-# resource's name field must match the portal_server_manifest_base_url value in
-# this env's .tfvars!
-resource "google_storage_bucket_object" "portal_server_global_manifest" {
-  name         = "portal-server/global-manifest.json"
-  bucket       = var.manifest_bucket
-  content_type = "application/json"
-  content = jsonencode({
-    format = 1
-    # We're cheating here by listing the same bucket twice, but the other env
-    # will consult a totally different portal server global manifest.
-    facilitator-sum-part-bucket = "gs://${google_storage_bucket.sum_part_output.name}"
-    pha-sum-part-bucket         = "gs://${google_storage_bucket.sum_part_output.name}"
-  })
-}
 
 resource "kubernetes_namespace" "tester" {
   metadata {
@@ -232,7 +185,7 @@ resource "kubernetes_deployment" "integration-tester" {
   for_each = var.ingestor_pairs
 
   metadata {
-    name      = "integration-tester-${each.value.ingestor}"
+    name      = "integration-tester-${each.value.locality}-${each.value.ingestor}"
     namespace = kubernetes_namespace.tester.metadata[0].name
   }
 
