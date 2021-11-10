c92abbd0fb2a2f80492fdabe69bcd6e9b203fe67
Brandon Pitman <brandon@letsencrypt.org>
2021-11-10 14:33:09 -0800

    Add methods to get all keys to storage/test's Key implementation.

--- key-rotator/storage/test/key.go
+++ key-rotator/storage/test/key.go
@@ -9,44 +9,51 @@ import (
 )
 
 // NewKey returns a Key which stores keys in memory only, suitable for testing.
-func NewKey() storage.Key {
-	return inMemoryKey{
-		batchSigningKeys:     map[localityIngestor]key.Key{},
+func NewKey() Key {
+	return Key{
+		batchSigningKeys:     map[LocalityIngestor]key.Key{},
 		packetEncryptionKeys: map[string]key.Key{},
 	}
 }
 
-type inMemoryKey struct {
-	batchSigningKeys     map[localityIngestor]key.Key
+type Key struct {
+	batchSigningKeys     map[LocalityIngestor]key.Key
 	packetEncryptionKeys map[string]key.Key // locality -> key
 }
 
-type localityIngestor struct{ locality, ingestor string }
+// LocalityIngestor represents a (locality, ingestor) tuple.
+type LocalityIngestor struct{ locality, ingestor string }
 
-var _ storage.Key = inMemoryKey{} // verify key satisfies storage.Key
+var _ storage.Key = Key{} // verify key satisfies storage.Key
 
-func (k inMemoryKey) PutBatchSigningKey(ctx context.Context, locality, ingestor string, key key.Key) error {
-	k.batchSigningKeys[localityIngestor{locality, ingestor}] = key
+// Standard storage.Key functions.
+func (k Key) PutBatchSigningKey(ctx context.Context, locality, ingestor string, key key.Key) error {
+	k.batchSigningKeys[LocalityIngestor{locality, ingestor}] = key
 	return nil
 }
 
-func (k inMemoryKey) PutPacketEncryptionKey(ctx context.Context, locality string, key key.Key) error {
+func (k Key) PutPacketEncryptionKey(ctx context.Context, locality string, key key.Key) error {
 	k.packetEncryptionKeys[locality] = key
 	return nil
 }
 
-func (k inMemoryKey) GetBatchSigningKey(ctx context.Context, locality, ingestor string) (key.Key, error) {
-	bsk, ok := k.batchSigningKeys[localityIngestor{locality, ingestor}]
+func (k Key) GetBatchSigningKey(ctx context.Context, locality, ingestor string) (key.Key, error) {
+	bsk, ok := k.batchSigningKeys[LocalityIngestor{locality, ingestor}]
 	if !ok {
 		return key.Key{}, fmt.Errorf("no batch signing key stored for (%q, %q)", locality, ingestor)
 	}
 	return bsk, nil
 }
 
-func (k inMemoryKey) GetPacketEncryptionKey(ctx context.Context, locality string) (key.Key, error) {
+func (k Key) GetPacketEncryptionKey(ctx context.Context, locality string) (key.Key, error) {
 	pek, ok := k.packetEncryptionKeys[locality]
 	if !ok {
 		return key.Key{}, fmt.Errorf("no packet encryption key stored for %q", locality)
 	}
 	return pek, nil
 }
+
+// Test-only functions.
+func (k Key) BatchSigningKeys() map[LocalityIngestor]key.Key { return k.batchSigningKeys }
+
+func (k Key) PacketEncryptionKeys() map[string]key.Key { return k.packetEncryptionKeys }
