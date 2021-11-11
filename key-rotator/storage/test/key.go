424546ae6983ff99feea5b9e7cd88909fa59c76f
Brandon Pitman <brandon@letsencrypt.org>
2021-11-10 19:54:35 -0800

    Implement Key Rotator binary.

--- key-rotator/storage/test/key.go
+++ key-rotator/storage/test/key.go
@@ -3,41 +3,49 @@ package test
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/abetterinternet/prio-server/key-rotator/key"
 	"github.com/abetterinternet/prio-server/key-rotator/storage"
 )
 
 // NewKey returns a Key which stores keys in memory only, suitable for testing.
-func NewKey() Key {
-	return Key{
+func NewKey() *Key {
+	return &Key{
 		batchSigningKeys:     map[LocalityIngestor]key.Key{},
 		packetEncryptionKeys: map[string]key.Key{},
 	}
 }
 
 type Key struct {
+	mu                   sync.Mutex // protects all fields
 	batchSigningKeys     map[LocalityIngestor]key.Key
 	packetEncryptionKeys map[string]key.Key // locality -> key
 }
 
 // LocalityIngestor represents a (locality, ingestor) tuple.
-type LocalityIngestor struct{ locality, ingestor string }
+type LocalityIngestor struct{ Locality, Ingestor string }
 
-var _ storage.Key = Key{} // verify key satisfies storage.Key
+var _ storage.Key = &Key{} // verify key satisfies storage.Key
 
-// Standard storage.Key functions.
-func (k Key) PutBatchSigningKey(ctx context.Context, locality, ingestor string, key key.Key) error {
+// Standard storage.Key functions. Safe for concurrent access from multiple goroutines.
+func (k *Key) PutBatchSigningKey(ctx context.Context, locality, ingestor string, key key.Key) error {
+	k.mu.Lock()
+	defer k.mu.Unlock()
 	k.batchSigningKeys[LocalityIngestor{locality, ingestor}] = key
 	return nil
 }
 
-func (k Key) PutPacketEncryptionKey(ctx context.Context, locality string, key key.Key) error {
+func (k *Key) PutPacketEncryptionKey(ctx context.Context, locality string, key key.Key) error {
+	k.mu.Lock()
+	defer k.mu.Unlock()
 	k.packetEncryptionKeys[locality] = key
 	return nil
 }
 
-func (k Key) GetBatchSigningKey(ctx context.Context, locality, ingestor string) (key.Key, error) {
+func (k *Key) GetBatchSigningKey(ctx context.Context, locality, ingestor string) (key.Key, error) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
 	bsk, ok := k.batchSigningKeys[LocalityIngestor{locality, ingestor}]
 	if !ok {
 		return key.Key{}, fmt.Errorf("no batch signing key stored for (%q, %q)", locality, ingestor)
@@ -45,7 +53,9 @@ func (k Key) GetBatchSigningKey(ctx context.Context, locality, ingestor string) 
 	return bsk, nil
 }
 
-func (k Key) GetPacketEncryptionKey(ctx context.Context, locality string) (key.Key, error) {
+func (k *Key) GetPacketEncryptionKey(ctx context.Context, locality string) (key.Key, error) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
 	pek, ok := k.packetEncryptionKeys[locality]
 	if !ok {
 		return key.Key{}, fmt.Errorf("no packet encryption key stored for %q", locality)
@@ -53,7 +63,7 @@ func (k Key) GetPacketEncryptionKey(ctx context.Context, locality string) (key.K
 	return pek, nil
 }
 
-// Test-only functions.
-func (k Key) BatchSigningKeys() map[LocalityIngestor]key.Key { return k.batchSigningKeys }
+// Test-only functions. Not goroutine-safe.
+func (k *Key) BatchSigningKeys() map[LocalityIngestor]key.Key { return k.batchSigningKeys }
 
-func (k Key) PacketEncryptionKeys() map[string]key.Key { return k.packetEncryptionKeys }
+func (k *Key) PacketEncryptionKeys() map[string]key.Key { return k.packetEncryptionKeys }
--- key-rotator/storage/test/manifest.go
+++ key-rotator/storage/test/manifest.go
@@ -4,6 +4,7 @@ package test
 
 import (
 	"context"
+	"sync"
 
 	"github.com/abetterinternet/prio-server/key-rotator/manifest"
 	"github.com/abetterinternet/prio-server/key-rotator/storage"
@@ -12,76 +13,66 @@ import (
 // NewManifest returns a Manifest which stores manifests in memory only,
 // suitable for testing.
 func NewManifest() *Manifest {
-	return &Manifest{dspInfos: map[string]*dspInfo{}}
+	return &Manifest{
+		dspManifests: map[string]manifest.DataShareProcessorSpecificManifest{},
+		dspPutCount:  map[string]int{},
+	}
 }
 
 type Manifest struct {
-	dspInfos map[string]*dspInfo
+	mu sync.Mutex // protects all fields
+
+	dspManifests map[string]manifest.DataShareProcessorSpecificManifest
+	dspPutCount  map[string]int
 
 	ingestorManifest *manifest.IngestorGlobalManifest
 	ingestorPutCount int
 }
 
-type dspInfo struct {
-	manifest manifest.DataShareProcessorSpecificManifest
-	putCount int
-}
-
 var _ storage.Manifest = &Manifest{} // verify *Manifest satisfies storage.Manifest
 
-// PutDataShareProcessorSpecificManifest writes the provided manifest for
-// the provided share processor name in the writer's backing storage, or
-// returns an error on failure.
+// Standard storage.Manifest functions. Safe for concurrent access from multiple goroutines.
 func (m *Manifest) PutDataShareProcessorSpecificManifest(_ context.Context, dspName string, manifest manifest.DataShareProcessorSpecificManifest) error {
-	di := m.dspInfos[dspName]
-	if di == nil {
-		di = &dspInfo{}
-		m.dspInfos[dspName] = di
-	}
-	di.manifest = manifest
-	di.putCount++
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.dspManifests[dspName] = manifest
+	m.dspPutCount[dspName]++
 	return nil
 }
 
-// PutIngestorGlobalManifest writes the provided manifest to the writer's
-// backing storage, or returns an error on failure.
 func (m *Manifest) PutIngestorGlobalManifest(_ context.Context, manifest manifest.IngestorGlobalManifest) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.ingestorManifest = &manifest
 	m.ingestorPutCount++
 	return nil
 }
 
-// GetDataShareProcessorSpecificManifest gets the specific manifest for the
-// specified data share processor and returns it, if it exists and is
-// well-formed. If the manifest does not exist, an error wrapping
-// storage.ErrObjectNotExist will be returned.
-func (m Manifest) GetDataShareProcessorSpecificManifest(_ context.Context, dspName string) (manifest.DataShareProcessorSpecificManifest, error) {
-	if di := m.dspInfos[dspName]; di != nil {
-		return di.manifest, nil
+func (m *Manifest) GetDataShareProcessorSpecificManifest(_ context.Context, dspName string) (manifest.DataShareProcessorSpecificManifest, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if manifest, ok := m.dspManifests[dspName]; ok {
+		return manifest, nil
 	}
 	return manifest.DataShareProcessorSpecificManifest{}, storage.ErrObjectNotExist
 }
 
-// GetDataShareProcessorSpecificManifestPutCount returns how many times
-// PutDataShareProcessorSpecificManifest has been called for the given data
-// share processor name.
-func (m Manifest) GetDataShareProcessorSpecificManifestPutCount(dspName string) int {
-	if di := m.dspInfos[dspName]; di != nil {
-		return di.putCount
-	}
-	return 0
-}
-
-// GetIngestorGlobalManifest gets the ingestor global manifest, if it
-// exists and is well-formed. If the manifest does not exist, an error
-// wrapping storage.ErrObjectNotExist will be returned.
-func (m Manifest) GetIngestorGlobalManifest(ctx context.Context) (manifest.IngestorGlobalManifest, error) {
+func (m *Manifest) GetIngestorGlobalManifest(ctx context.Context) (manifest.IngestorGlobalManifest, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	if m.ingestorManifest != nil {
 		return *m.ingestorManifest, nil
 	}
 	return manifest.IngestorGlobalManifest{}, storage.ErrObjectNotExist
 }
 
-// GetIngestorGlobalManifestPutCount returns how many times
-// PutIngestorGlobalManifest has been called.
-func (m Manifest) GetIngestorGlobalManifestPutCount() int { return m.ingestorPutCount }
+// Test-only functions. NOT goroutine-safe.
+func (m *Manifest) GetDataShareProcessorSpecificManifests() map[string]manifest.DataShareProcessorSpecificManifest {
+	return m.dspManifests
+}
+
+func (m *Manifest) GetDataShareProcessorSpecificManifestPutCount(dspName string) int {
+	return m.dspPutCount[dspName]
+}
+
+func (m *Manifest) GetIngestorGlobalManifestPutCount() int { return m.ingestorPutCount }
