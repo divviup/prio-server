1f71ba4d8081974218c9163aa6343210211d0536
Brandon Pitman <brandon@letsencrypt.org>
2021-12-01 10:58:40 -0800

    Fixup code review comments.

--- key-rotator/main.go
+++ key-rotator/main.go
@@ -370,10 +370,10 @@ func writeManifests(
 		ingestor, oldManifest, newManifest := ingestor, oldManifest, newManifestByIngestor[ingestor]
 		eg.Go(func() error {
 			if oldManifest.Equal(newManifest) {
-				log.Debug().Msgf("Skipping write for manifest for (%q, %q): key unchanged", locality, ingestor)
+				log.Debug().Str("locality", locality).Str("ingestor", ingestor).Msgf("Skipping write for manifest for (%q, %q): key unchanged", locality, ingestor)
 				return nil
 			}
-			log.Info().Msgf("Writing manifest for (%q, %q): %s", locality, ingestor, newManifest.Diff(oldManifest))
+			log.Info().Str("locality", locality).Str("ingestor", ingestor).Msgf("Writing manifest for (%q, %q): %s", locality, ingestor, newManifest.Diff(oldManifest))
 			if err := manifestStore.PutDataShareProcessorSpecificManifest(ctx, dspName(locality, ingestor), newManifest); err != nil {
 				return fmt.Errorf("couldn't write manifest for (%q, %q): %w", locality, ingestor, err)
 			}
