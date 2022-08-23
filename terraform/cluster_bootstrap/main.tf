8d2ec888790d90ce90569dae4aa35b5b02776121
Tim Geoghegan <timg@letsencrypt.org>
2022-08-23 09:44:58 -0700

    Bump google and google-beta providers to 4.33.0

--- terraform/cluster_bootstrap/main.tf
+++ terraform/cluster_bootstrap/main.tf
@@ -108,12 +108,12 @@ terraform {
     google = {
       source = "hashicorp/google"
       # Keep this version in sync with provider google-beta
-      version = "4.32.0"
+      version = "4.33.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
       # Keep this version in sync with provider google
-      version = "4.32.0"
+      version = "4.33.0"
     }
     external = {
       source  = "hashicorp/external"
