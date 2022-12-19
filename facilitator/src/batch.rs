0ca6314869a0d61e7a21321a9dabc655d1cf3e09
David Cook <dcook@letsencrypt.org>
2022-12-19 11:07:02 -0600

    Clippy fixes (#2321)
    * Clippy fixes
    
    * Clippy fixes in tests

--- facilitator/src/batch.rs
+++ facilitator/src/batch.rs
@@ -56,7 +56,7 @@ impl Batch<ValidationHeader, ValidationPacket> {
             aggregation_name,
             batch_id,
             date,
-            &format!("validity_{}", if is_first { 0 } else { 1 }),
+            &format!("validity_{}", i32::from(!is_first)),
         )
     }
 }
@@ -77,16 +77,12 @@ impl Batch<SumPart, InvalidPacket> {
             aggregation_start.format(AGGREGATION_DATE_FORMAT),
             aggregation_end.format(AGGREGATION_DATE_FORMAT)
         );
-        let filename = format!("sum_{}", if is_first { 0 } else { 1 });
+        let filename = format!("sum_{}", i32::from(!is_first));
 
         Self {
             header_path: format!("{}.{}", batch_path, filename),
             signature_path: format!("{}.{}.sig", batch_path, filename),
-            packet_file_path: format!(
-                "{}.invalid_uuid_{}.avro",
-                batch_path,
-                if is_first { 0 } else { 1 }
-            ),
+            packet_file_path: format!("{}.invalid_uuid_{}.avro", batch_path, i32::from(!is_first)),
 
             phantom_header: PhantomData,
             phantom_packet: PhantomData,
@@ -899,7 +895,7 @@ mod tests {
             END_TIMESTAMP.format(DATE_FORMAT),
             BATCH_ID.to_hyphenated()
         );
-        let idx = if is_first { 0 } else { 1 };
+        let idx = i32::from(!is_first);
         let filenames = if single_object_write {
             vec![format!("validity_{}.sig", idx)]
         } else {
@@ -1007,7 +1003,7 @@ mod tests {
             START_TIMESTAMP.format(AGGREGATION_DATE_FORMAT),
             END_TIMESTAMP.format(AGGREGATION_DATE_FORMAT)
         );
-        let idx = if is_first { 0 } else { 1 };
+        let idx = i32::from(!is_first);
         let filenames = if single_object_write {
             vec![format!("sum_{}.sig", idx)]
         } else {
