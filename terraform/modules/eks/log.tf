ad4a38d55226bb2a2c57added605b2e5216b2e1f
David Cook <dcook@letsencrypt.org>
2022-12-21 08:29:14 -0600

    Update to AWS for Fluent Bit 2.29.0 (#2322)

--- terraform/modules/eks/log.tf
+++ terraform/modules/eks/log.tf
@@ -172,7 +172,7 @@ resource "kubernetes_daemonset" "fluent_bit" {
 
         container {
           name              = "fluent-bit"
-          image             = "amazon/aws-for-fluent-bit:2.28.3"
+          image             = "amazon/aws-for-fluent-bit:2.29.0"
           image_pull_policy = "Always"
 
           env {
